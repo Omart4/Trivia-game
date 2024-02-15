c5d3e3ffe5c282f45070399504834155e0d5aa93
Omar Elkony <89548036+Omart4@users.noreply.github.com>
2024-02-15 02:35:48 +0200

    First day

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Logo from "../assets/pngtree-trivia-night-speech-bubble-png-image_6568577.png";
+import CategorySelect from "../Components/CategorySelect";
+import '../Styles/MainPage.css'
+
+const MainPage = (props) => {
+  //States
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [ready, setReady] = useState(false);
+
+  const baseUrl = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`;
+
+  return (
+    <div className="main">
+      <img src={Logo} alt="logo" />
+      <div className="inputs">
+        <label htmlFor="amount">Amount of questions:</label>
+        <input
+          onChange={(e) => {
+            setAmount(e.target.value);
+            if (e.target.value === 0 || e.target.value === "") {
+              setReady(false);
+            } else {
+              setReady(true);
+            }
+          }}
+          type="number"
+          id="amount"
+          name="amount"
+        />
+
+        <label htmlFor="category">Category:</label>
+        <CategorySelect setCategory={setCategory} />
+
+        <label htmlFor="difficulty">Difficulty:</label>
+        <select
+          onChange={(e) => setDifficulty(e.target.value)}
+          name="difficulty"
+          id="difficulty"
+        >
+          <option selected value="">
+            Any difficulty
+          </option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <button
+        onClick={() => {
+          console.log(baseUrl);
+          if (!ready) {
+            alert("Choose an amount of questions");
+          }
+        }}
+        className="start"
+      >
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default MainPage;
